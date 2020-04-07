ee6e3615ef1413c29cd2e6aaa9f2a22236c2d622
Kayden <kayden.adams@colorado.edu>
2020-04-07 11:26:33 -0600

    Initial push to get GitPages to work.

--- main.bundle.js
+++ main.bundle.js
@@ -44,7 +44,106 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-	"use strict";
+	'use strict';
+
+	// Bootstrapped from https://www.sitepoint.com/simple-javascript-quiz/
+	(function () {
+	  function buildQuiz() {
+	    // variable to store the HTML output
+	    var output = [];
+
+	    // for each question...
+	    myQuestions.forEach(function (currentQuestion, questionNumber) {
+
+	      // variable to store the list of possible answers
+	      var answers = [];
+
+	      // and for each available answer...
+	      for (letter in currentQuestion.answers) {
+
+	        // ...add an HTML radio button
+	        answers.push('<label>\n                <input type="radio" name="question' + questionNumber + '" value="' + letter + '">\n                ' + letter + ' :\n                ' + currentQuestion.answers[letter] + '\n              </label>');
+	      }
+
+	      // add this question and its answers to the output
+	      output.push('<div class="question"> ' + currentQuestion.question + ' </div>\n            <div class="answers"> ' + answers.join('') + ' </div>');
+	    });
+
+	    // finally combine our output list into one string of HTML and put it on the page
+	    quizContainer.innerHTML = output.join('');
+	  }
+
+	  function showResults() {
+
+	    // gather answer containers from our quiz
+	    var answerContainers = quizContainer.querySelectorAll('.answers');
+
+	    // keep track of user's answers
+	    var numCorrect = 0;
+
+	    // for each question...
+	    myQuestions.forEach(function (currentQuestion, questionNumber) {
+
+	      // find selected answer
+	      var answerContainer = answerContainers[questionNumber];
+	      var selector = 'input[name=question' + questionNumber + ']:checked';
+	      var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+	      // if answer is correct
+	      if (userAnswer === currentQuestion.correctAnswer) {
+	        // add to the number of correct answers
+	        numCorrect++;
+
+	        // color the answers green
+	        answerContainers[questionNumber].style.color = 'lightgreen';
+	      }
+	      // if answer is wrong or blank
+	      else {
+	          // color the answers red
+	          answerContainers[questionNumber].style.color = 'red';
+	        }
+	    });
+
+	    // show number of correct answers out of total
+	    resultsContainer.innerHTML = numCorrect + ' out of ' + myQuestions.length;
+	  }
+
+	  var quizContainer = document.getElementById('quiz');
+	  var resultsContainer = document.getElementById('results');
+	  var submitButton = document.getElementById('submit');
+	  var myQuestions = [{
+	    question: "Who invented JavaScript?",
+	    answers: {
+	      a: "Douglas Crockford",
+	      b: "Sheryl Sandberg",
+	      c: "Brendan Eich"
+	    },
+	    correctAnswer: "c"
+	  }, {
+	    question: "Which one of these is a JavaScript package manager?",
+	    answers: {
+	      a: "Node.js",
+	      b: "TypeScript",
+	      c: "npm"
+	    },
+	    correctAnswer: "c"
+	  }, {
+	    question: "Which tool can you use to ensure code quality?",
+	    answers: {
+	      a: "Angular",
+	      b: "jQuery",
+	      c: "RequireJS",
+	      d: "ESLint"
+	    },
+	    correctAnswer: "d"
+	  }];
+
+	  // Kick things off
+	  buildQuiz();
+
+	  // Event listeners
+	  submitButton.addEventListener('click', showResults);
+	})();
 
 /***/ })
 /******/ ]);
